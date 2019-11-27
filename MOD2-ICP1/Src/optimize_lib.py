f383b0af80921addf9ea4e00a4b9643645728f8e
oalhaiki <54451383+oalhaiki@users.noreply.github.com>
2019-11-26 22:23:46 -0600

    Create optimize_lib.py

---
+++ MOD2-ICP1/Src/optimize_lib.py
@@ -0,0 +1,29 @@
+input_graph_def = tf.GraphDef()
+
+with tf.gfile.Open(output_frozen_graph_name, "r") as f:
+
+    data = f.read()
+
+    input_graph_def.ParseFromString(data)
+
+
+
+output_graph_def = optimize_for_inference_lib.optimize_for_inference(
+
+        input_graph_def,
+
+        ["I"], # an array of the input node(s)
+
+        ["O"], # an array of output nodes
+
+        tf.float32.as_datatype_enum)
+
+
+
+# Save the optimized graph
+
+
+
+f = tf.gfile.FastGFile(output_optimized_graph_name, "w")
+
+f.write(output_graph_def.SerializeToString())
